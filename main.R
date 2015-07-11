3b36b759699295ec6a24a28bcdc93f06c315028e
Jim Pfleger <jpfleger@acm.org>
2015-07-11 09:09:11 -0700

    Prepare data frame

---
+++ main.R
@@ -0,0 +1,13 @@
+# Load complete file
+raw <- read.delim('data/household_power_consumption.txt', sep = ';', na.strings = '?',
+                  colClasses = c('character', 'character',
+                                 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+
+# Grab just the two dates we need (dates are in D/M/Y format)
+power <- raw[(raw$Date == '1/2/2007' | raw$Date == '2/2/2007'), ]
+
+# Save some memory
+remove(raw)
+
+# Clean up column types
+power$DateTime <- strptime(paste(power$Date, power$Time), '%d/%m/%Y %T')
