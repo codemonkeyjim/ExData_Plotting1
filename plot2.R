0e33bfe26f94e8bf1e7cd505737f1c04ef16ec34
Jim Pfleger <jpfleger@acm.org>
2015-07-11 09:51:40 -0700

    Move data-reading code into each plot per instructions

--- plot2.R
+++ plot2.R
@@ -1,4 +1,16 @@
-# Assumes data is loaded into power (see main.R)
+# Load complete file
+raw <- read.delim('data/household_power_consumption.txt', sep = ';', na.strings = '?',
+                  colClasses = c('character', 'character',
+                                 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+
+# Grab just the two dates we need (dates are in D/M/Y format)
+power <- raw[(raw$Date == '1/2/2007' | raw$Date == '2/2/2007'), ]
+
+# Save some memory
+remove(raw)
+
+# Clean up column types
+power$DateTime <- strptime(paste(power$Date, power$Time), '%d/%m/%Y %T')
 
 png('plot2.png')
 plot(power$DateTime, power$Global_active_power,
